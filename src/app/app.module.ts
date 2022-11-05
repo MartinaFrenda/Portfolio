6bf1eacc1fdaf4f30da26ce9f14584821e3e46b8
Martina <martufrenda18@gmail.com>
2022-11-04 21:22:22 -0300

    router listoo y sin errores :)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes/header/header.component';
 import { FooterComponent } from './componentes/footer/footer.component';
